bbaa3b8ddf3203fbacc9a98dfdd13e4d5b28d62c
DyPeterson <dylan.peterson17@gmail.com>
2022-05-27 10:03:58 -0700

    [BUGFIX] Allow users to land on /add_stats without a POST method

--- main.py
+++ main.py
@@ -65,40 +65,43 @@ def see_stats():
 
     return response_json, 200, response_headers
 
-@app.route('/add_stats', methods=['POST'])
+@app.route('/add_stats', methods=['GET','POST'])
 def add_stats():
     """
     Function to add a superhero to the database.
     """
     global super_df
-    try:
-        added_heroes = []
-        rejected_heroes = []
-        # :(
-        data = request.json
-        for hero in data:
-            if ('name' in hero) and ('superpower' in hero) and ('weakness' in hero):
-                logger.info(f'Adding a hero to the hideout, welcome {hero}')
-                index = hero['name']
-                super_df.loc[hero['name']] = hero
-                added_heroes.append(hero)
-            else:
-                logger.info(f'Rejecting {hero}')
-                rejected_heroes.append(hero)
-        logger.info(f'Added {len(added_heroes)} heroes & Rejected {len(rejected_heroes)} heroes')
-        response_json = {
-            'heroes_added': len(added_heroes),
-            'result': added_heroes,
-            'rejects': rejected_heroes,
-        }
+    if request.method == 'GET':
+        return 'Please supply a POST to this endpoint to add a hero to the database'
+    else:
+        try:
+            added_heroes = []
+            rejected_heroes = []
+            # :(
+            data = request.json
+            for hero in data:
+                if ('name' in hero) and ('superpower' in hero) and ('weakness' in hero):
+                    logger.info(f'Adding a hero to the hideout, welcome {hero}')
+                    index = hero['name']
+                    super_df.loc[hero['name']] = hero
+                    added_heroes.append(hero)
+                else:
+                    logger.info(f'Rejecting {hero}')
+                    rejected_heroes.append(hero)
+            logger.info(f'Added {len(added_heroes)} heroes & Rejected {len(rejected_heroes)} heroes')
+            response_json = {
+                'heroes_added': len(added_heroes),
+                'result': added_heroes,
+                'rejects': rejected_heroes,
+            }
 
-        response_header = {
-            'content-type': 'application/json'
-        }
+            response_header = {
+                'content-type': 'application/json'
+            }
 
-        return response_json, 200, response_header
-    except Exception as error:
-        return {'status': 'error', 'error_msg': str(error)}, 400, response_header
+            return response_json, 200, response_header
+        except Exception as error:
+            return {'status': 'error', 'error_msg': str(error)}, 400, {'content-type': 'application/json'}
 
 if __name__ == '__main__':
     app.run('0.0.0.0', 5050)
