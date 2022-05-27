642d9eea4e1d95a0a662ad17191039830d8aec01
DyPeterson <dylan.peterson17@gmail.com>
2022-05-27 09:07:42 -0700

    add query conditionals to see_stats(GET) function

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import logging
 import pandas as pd
 from flask import Flask, request
+
+logging.basicConfig(format='[%(levelname)-5s][%(asctime)s][%(module)s:%(lineno)04d] : %(message)s',
+                    level=logging.INFO,
+                    stream=sys.stderr)
+logger: logging.Logger = logging
 
 super_data = [
     {"name": '' , "superpower": '' , "weakness": '' },
@@ -14,7 +20,32 @@ app.config["db"] = super_df
 
 @app.route('/see_stats', methods=['GET'])
 def see_stats():
-    pass
+    """
+    Query with either name, superpower and/or weakness
+    """
+    global super_df
+    name = request.args.get('name', default=None)
+    superpower = request.args.get('superpower', default=None)
+    weakness = request.args.get('weakness', default=None)
+
+    if name is not None:
+        logger.info(f'name query for {name}')
+        results_df = super_df.loc[super_df['name'] == name]
+    else:
+        results_df = super_df
+    
+    if superpower is not None:
+        logger.info(f'superpower query for {superpower}')
+        results_df = super_df.loc[super_df['superpower'] == superpower]
+    else:
+        results_df = super_df
+
+    if weakness is not None:
+        logger.info(f'weakness query for {weakness}')
+        results_df = super_df.loc[super_df['weakness'] == weakness]
+    else:
+        results_df = super_df
+    
 
 @app.route('/add_stats', methods=['POST'])
 def add_stats():
