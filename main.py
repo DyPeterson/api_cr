00098ea28589115377c9b64a7cd78010a1da7a95
DyPeterson <dylan.peterson17@gmail.com>
2022-05-27 09:29:46 -0700

    add index page and finish return on see_stats

--- main.py
+++ main.py
@@ -8,15 +8,23 @@ logging.basicConfig(format='[%(levelname)-5s][%(asctime)s][%(module)s:%(lineno)0
 logger: logging.Logger = logging
 
 super_data = [
-    {"name": '' , "superpower": '' , "weakness": '' },
-    {"name": '' , "superpower": '' , "weakness": '' },
-    {"name": '' , "superpower": '' , "weakness": '' }
+    {"name": 'Friendly Fire' , "superpower": 'Energy Bolts' , "weakness": 'Aim' },
+    {"name": 'Batman' , "superpower": 'Money & Training' , "weakness": 'Trauma' },
+    {"name": 'The Hulk' , "superpower": 'Radiation' , "weakness": 'Mood' }
 ]
 super_df  = pd.DataFrame(super_data)
 super_df.set_index(keys="name", drop=False, inplace=True)
 
 app = Flask(__name__)
 app.config["db"] = super_df
+
+@app.route("/")
+def index():
+    index_str = """
+    Welcome to heroes hideout, please orient yourself to '/see_stats' or '/add_stats'
+    """
+    return '', 200
+
 
 @app.route('/see_stats', methods=['GET'])
 def see_stats():
@@ -45,10 +53,26 @@ def see_stats():
         results_df = super_df.loc[super_df['weakness'] == weakness]
     else:
         results_df = super_df
+
+    response_json = {
+        'query_name': name,
+        'query_superpower': superpower,
+        'query_weakness': weakness,
+        'result': results_df.to_dict(orient='records'),
+    }
     
+    response_headers = {
+        'content-type': 'application/json'
+    }
+
+    return response_json, 200, response_headers
 
 @app.route('/add_stats', methods=['POST'])
 def add_stats():
+    """
+    Function to add a superhero to the database.
+    """
+    global super_df
     pass
 
 if __name__ == '__main__':
